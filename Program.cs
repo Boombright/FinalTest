1431eee2902d263bfbcc475d6352b97f9b91af67
BoomZa Nightcore <krittameth193@gmail.com>
2021-06-13 16:07:28 +0700

    ข้อ1

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace finalTEST
+{
+    class Program
+    {
+        //ข้อ1
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+            int num5 = int.Parse(Console.ReadLine());
+            int num6 = int.Parse(Console.ReadLine());
+            int num7 = int.Parse(Console.ReadLine());
+            int num8 = int.Parse(Console.ReadLine());
+            int num9 = int.Parse(Console.ReadLine());
+            int num10 = int.Parse(Console.ReadLine());
+
+            Console.ReadLine();
+
+            Console.Write(num1);
+            Console.Write(" ");
+            Console.Write(num2);
+            Console.Write(" ");
+            Console.Write(num3);
+            Console.WriteLine();
+            Console.Write(num4);
+            Console.Write(" ");
+            Console.Write(num5);
+            Console.Write(" ");
+            Console.Write(num6);
+            Console.WriteLine();
+            Console.Write(num7);
+            Console.Write(" ");
+            Console.Write(num8);
+            Console.Write(" ");
+            Console.Write(num9);
+
+            Console.ReadLine();
+
+            if(num1 == 0)
+            {
+                num1 = num10;
+            }
+            else if (num2 == 0)
+            {
+                num2 = num10;
+            }
+            else if (num3 == 0)
+            {
+                num3 = num10;
+            }
+            else if (num4 == 0)
+            {
+                num4 = num10;
+            }
+            else if (num5 == 0)
+            {
+                num5 = num10;
+            }
+            else if (num6 == 0)
+            {
+                num6 = num10;
+            }
+            else if (num7 == 0)
+            {
+                num7 = num10;
+            }
+            else if (num8 == 0)
+            {
+                num8 = num10;
+            }
+            else if (num9 == 0)
+            {
+                num9 = num10;
+            }
+
+            Console.ReadLine();
+            Console.ReadLine();
+            Console.ReadLine();
+
+            Console.Write(num1);
+            Console.Write(" ");
+            Console.Write(num2);
+            Console.Write(" ");
+            Console.Write(num3);
+            Console.WriteLine();
+            Console.Write(num4);
+            Console.Write(" ");
+            Console.Write(num5);
+            Console.Write(" ");
+            Console.Write(num6);
+            Console.WriteLine();
+            Console.Write(num7);
+            Console.Write(" ");
+            Console.Write(num8);
+            Console.Write(" ");
+            Console.Write(num9);
+        }
+    }
+}
