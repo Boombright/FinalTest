58e3529826ce0ca4dae6fa242f2f417f2e919c64
BoomZa Nightcore <krittameth193@gmail.com>
2021-06-13 17:19:24 +0700

    ข้อ3

---
+++ ข้อ3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FinalTest_pt3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ข้อ3
+            static void Main(string[] args)
+            {
+                string input = Console.ReadLine();
+                while (input != "End")
+                {
+                    input = Console.ReadLine();
+                }
+                Console.WriteLine("Find what?: ");
+                string mode = Console.ReadLine();
+                if (mode == "FindMax")
+                {
+                    byte a = Math.Max(input, input);
+                    Console.WriteLine(a);
+                }
+                else if (mode == "FindMin")
+                {
+                    byte b = Math.Min(input, input);
+                    Console.WriteLine(b);
+                }
+                else if (mode == "FindMean")
+                {
+                    byte c = Math.Min(input, input);
+                    Console.WriteLine(c);
+                }
+                else
+                    Console.WriteLine("Invalid Mode");
+
+            }
+        }
+    }
+}
