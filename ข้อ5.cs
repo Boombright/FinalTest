9b2a4cc87178e159ac7da23b0232b6c81f72f4da
BoomZa Nightcore <krittameth193@gmail.com>
2021-06-13 17:22:16 +0700

    ข้อ5

---
+++ ข้อ5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FInalTest_pt5
+{
+    class Program
+    {
+        //ข้อ5
+        struct ItemProperty
+        {
+            public string ItemName;
+            public string ItemType;
+        }
+
+        static void Main(string[] args)
+        {
+            int ItemNumber = int.Parse(Console.ReadLine());
+            ItemProperty[] Item = new ItemProperty[ItemNumber];
+            for (int i = 0; i <= ItemNumber; i++)
+            {
+                Item[i].ItemName = Console.ReadLine();
+                Item[i].ItemType = Console.ReadLine();
+            }
+
+            string mode = Console.ReadLine();
+            if(mode == "Showall")
+            {
+                Console.WriteLine(Item);
+            }
+        }
+    }
+}
